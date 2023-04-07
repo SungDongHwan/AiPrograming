02fcc731b386bbc0e141a40d6572d511aff6dd8a
성동환 <122775098+SungDongHwan@users.noreply.github.com>
2023-04-07 18:25:23 +0900

    AIProgramming

---
+++ day1.py
@@ -0,0 +1,31 @@
+# Contents - day1
+# Values and types
+
+print("hello World!")
+
+
+type("hello World!")
+type((1, 0, 0))
+for i in[1,2,3]:
+    print(i)
+list = [1,2,3]
+if 2 in list:
+    print('contains')
+s = 'hello'
+if 'h' in s:
+    print('contain')
+
+s1 = '''
+가나다라 마바사
+'''
+n = ("5")
+n +'3'
+
+str  = "pythin is good"
+for i in range(len(str)):
+    print(str[i])
+n = 5
+print(str[:n] , str[n:], sep='\n')
+print(*list)
+'answer :{:.2f}'.format(3.14158)
+print("kim", 100, sep=',')
